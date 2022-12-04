960a1f3358433d07581cae0c25f61b8705c58d95
饶兆锋 <245532242@qq.com>
2022-12-05 01:11:56 +0800

    新增des 加密方法

--- example/components.go
+++ example/components.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/raozhaofeng/beego/utils"
+)
+
 func main() {
-	
+	//	加密
+	data, err := utils.NewEncryption().DesEncrypt([]byte("1234512345123451234512345123451234512345"), []byte("abc11111"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data)
+	oldData, _ := utils.NewEncryption().DesDecrypt(data, []byte("abc11111"))
+
+	fmt.Println(string(oldData))
 }
